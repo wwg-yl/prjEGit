a6cd2d8f82e6362106e3a6bba329ecc42fd347d4
wangweiguo <43645474+wwg-yl@users.noreply.github.com>
2018-11-30 14:09:12 +0800

    Update Test.java

--- src/com/atguigu/test/Test.java
+++ src/com/atguigu/test/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    system.out.println("123")
-System.out.println("456")
+    System.out.println("123");
+System.out.println("456");
 	}
 
 }
